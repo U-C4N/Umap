e8d91720af5f6aed4557f085ef893948f8093f69
Umut CAN <78921017+U-C4N@users.noreply.github.com>
2025-06-04 22:53:05 +0300

    Add package version

--- umap/__init__.py
+++ umap/__init__.py
@@ -4,4 +4,7 @@ from .core.fetch import get_gdfs
 from .utils.drawing import add_frame
 from .cli import main as cli_main
 
+# Package version
+__version__ = "2.0"
+
 __all__ = ['plot', 'multiplot', 'Plot', 'Subplot', 'get_gdfs', 'add_frame', 'cli_main']
